d0a7d83f5824f18f4bb51f3b7d6c4b22e9b069e8
Maxim Tsvyk <162802460+rand-06@users.noreply.github.com>
2026-06-25 22:14:16 +0300

    update repo

--- Assets/veryAnnoyingScript.cs
+++ Assets/veryAnnoyingScript.cs
@@ -1,77 +1,50 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using KModkit;
+using Random = UnityEngine.Random;
 
 public class veryAnnoyingScript : MonoBehaviour {
 
-    public KMSelectable[] buttons = new KMSelectable[16];
-    public TextMesh[] texts = new TextMesh[16];
-    public MeshRenderer[] buttonMesh = new MeshRenderer[16];
-    public GameObject[] buttonObjects = new GameObject[16];
+    public KMSelectable[] buttons;
+    public TextMesh[] texts;
+    public MeshRenderer[] buttonMesh;
+    public GameObject[] buttonObjects;
     public TextMesh color;
     public KMBombModule Module;
     public MeshRenderer moduleMesh;
     public KMAudio Audio;
     public AudioClip[] clips = new AudioClip[3];
 
-    private int[] points = new int[16];
-    private int correctPressed = 0;
-    private bool secondStage = false;
-    private bool solved = false;
+    private int[] points = new int[9];
+    private int correctPressed ;
+    private bool secondStage;
+    private bool solved;
 
-    private int[] red = new int[16];
-    private int[] green = new int[16];
-    private int[] blue = new int[16];
-    private string[] colors = new string[16];
+    private int[] red = new int[9];
+    private int[] green = new int[9];
+    private int[] blue = new int[9];
+    private string[] colors = new string[9];
 
-    static int ModuleIdCounter = 0;
+    static int ModuleIdCounter;
     int ModuleId;
-
-    int max(int a, int b) { return a > b ? a : b; }
-    int min(int a, int b) { return a < b ? a : b; }
 
     int[] generateFirstStage()
     {
-        int[] numbers = new int[16];
-        int[] ans = new int[16];
-        string log = "Button points (stage 1):\n";
-        for (int i=0; i<16; i++)
-        {
-            numbers[i] = Random.Range(0, 65536);
-            ans[i] = 0;
-            int mask = 1;
-            for (int j=0; j < 16; j++)
-            {
-                if ((numbers[i] & mask) > 0) ans[i]++;
-                mask <<= 1;
-            }
-            texts[i].text = numbers[i].ToString();
-            log += ans[i].ToString() + " ";
-            if (i % 4 == 3) log += "\n";
-        }
-        Debug.Log(log);
+        int[] numbers = Enumerable.Range(0, 9).Select(_ => Random.Range(0, 65536)).ToArray();
+        for (int i = 0; i < 9; i++) texts[i].text = numbers[i].ToString();
+        int[] ans = Enumerable.Range(0, 9).Select(i => 
+            Enumerable.Range(0, 16).Select(x => (numbers[i] & 1 << x) > 0 ? 1 : 0).Sum()
+            ).ToArray();
+        Debug.Log("[Very Annoying Puzzle #" + ModuleId + "] Button points (stage 1): "
+                  + ans.Aggregate("", (a, b) => a + ", " + b));
         return ans;
     }
-
-    bool checkMin(int[] array, int index)
-    {
-        for (int i=0; i<array.Length; i++){if (array[i] < array[index]) return false;}
-        return true;
-    }
-    int mod(int a, int b)
-    {
-        while (a < 0) a += b;
-        while (a >= b) a -= b;
-        return a;
-    }
-
     int rgbToHsvSum(int r, int g, int b)
     {
-        int maxVal = max(max(r, g), b);
-        int minVal = min(min(r, g), b);
+        int maxVal = new[] { r, g, b }.Max();
+        int minVal = new[] { r, g, b }.Min();
         int delta = maxVal - minVal;
-
+        
         int h = 0;
         int s = 0;
         if (delta != 0)
@@ -79,16 +52,10 @@ public class veryAnnoyingScript : MonoBehaviour {
             if (maxVal == r) h = 60 * (g - b) / delta;
             else if (maxVal == g) h = 60 * (b - r) / delta +120;
             else h = 60 * (r - g) / delta +240;
-            h = mod(h, 360);
+            h = (h % 360 + 360) % 360;
         }
         if (maxVal != 0) s = 255 * delta / maxVal;
         return h + s + maxVal;
-    }
-    
-    string dectohex(int num)
-    {
-        string hex = "0123456789ABCDEF";
-        return hex[num / 16].ToString() + hex[num % 16].ToString();
     }
 
     void nextStage()
@@ -96,8 +63,8 @@ public class veryAnnoyingScript : MonoBehaviour {
         if (secondStage)
         {
             Module.HandlePass();
-            string final = "YOUDIDITGOODJOB!";
-            for (int i=0; i<16; i++) {
+            string final = "VAPSOLVED";
+            for (int i=0; i<9; i++) {
                 texts[i].text = final[i].ToString();
                 buttonMesh[i].material.color = new Color(0, 178 / 255f, 0);
                 buttonObjects[i].SetActive(true);
@@ -116,31 +83,28 @@ public class veryAnnoyingScript : MonoBehaviour {
 
     int[] generateSecondStage()
     {
-        int[] ans = new int[16];
-        string log = "Button points (stage 2):\n";
-        for (int i=0; i<16; i++)
+        int[] temp = Enumerable.Range(0, 9).Select(_ => Random.Range(0, 256*256*256)).ToArray();
+        red = temp.Select(x => x/256/256).ToArray();
+        green = temp.Select(x => x/256%256).ToArray();
+        blue = temp.Select(x => x%256).ToArray();
+        colors = temp.Select(x => "#"+x.ToString("X")).ToArray();
+        int[] ans = Enumerable.Range(0, 9).Select(i => rgbToHsvSum(255-red[i], 255 - green[i], 255 - blue[i])).ToArray();
+        Debug.Log("[Very Annoying Puzzle #" + ModuleId + "] Button points (stage 2): " + ans.Select(x=>x.ToString()).Aggregate((a, b) => a + ", " + b));
+        for (int i=0; i<9; i++)
         {
-            red[i] = Random.Range(0, 256);
-            green[i] = Random.Range(0, 256);
-            blue[i] = Random.Range(0, 256);
             texts[i].text = "";
             buttonMesh[i].material.color = new Color(red[i] / 255f, green[i] / 255f, blue[i] / 255f);
-            colors[i] = "#"+ dectohex(red[i]) + dectohex(green[i]) + dectohex(blue[i]);
-            ans[i] = rgbToHsvSum(255-red[i], 255 - green[i], 255 - blue[i]);
-            log += ans[i].ToString() + " ";
-            if (i % 4 == 3) log += "\n";
         }
-        Debug.Log(log);
         return ans;
     }
 
-    bool Press(int index)
+    void Press(int index)
     {
         if (!solved)
         {
             if (points[index] != 999)
             {
-                if (checkMin(points, index))
+                if (!Enumerable.Range(0, points.Length).Any(x=>points[x] < points[index]))
                 {
                     correctPressed++;
                     texts[index].text = "";
@@ -148,7 +112,7 @@ public class veryAnnoyingScript : MonoBehaviour {
                     if (secondStage) { buttonObjects[index].SetActive(false); }
 
 
-                    if (correctPressed == 16)
+                    if (correctPressed == 9)
                     {
                         nextStage();
                         Audio.PlaySoundAtTransform(clips[1].name, transform);
@@ -159,16 +123,13 @@ public class veryAnnoyingScript : MonoBehaviour {
                 {
                     Module.HandleStrike();
                     correctPressed = 0;
-                    for (int i=0; i<16; i++) buttonObjects[i].SetActive(true);
+                    for (int i=0; i<9; i++) buttonObjects[i].SetActive(true);
                     points = secondStage ? generateSecondStage() : generateFirstStage();
                     Audio.PlaySoundAtTransform(clips[2].name, transform);
                 }
             }
         }
-        return true;
     }
-
-
 
 	void Start () {
 
@@ -176,87 +137,49 @@ public class veryAnnoyingScript : MonoBehaviour {
         ModuleId++;
         color.text = "";
         points = generateFirstStage();
-
-        for (int i = 0; i < 16; i++) buttonMesh[i].material.color = new Color(104 / 255f, 46 / 255f, 46 / 255f);
+        for (int i = 0; i < 9; i++) buttonMesh[i].material.color = new Color(104 / 255f, 46 / 255f, 46 / 255f);
         moduleMesh.material.color = new Color(178 / 255f, 65 / 255f, 65 / 255f);
-
-        buttons[0].OnInteract += delegate () { return Press(0); };
-        buttons[1].OnInteract += delegate () { return Press(1); };
-        buttons[2].OnInteract += delegate () { return Press(2); };
-        buttons[3].OnInteract += delegate () { return Press(3); };
-        buttons[4].OnInteract += delegate () { return Press(4); };
-        buttons[5].OnInteract += delegate () { return Press(5); };
-        buttons[6].OnInteract += delegate () { return Press(6); };
-        buttons[7].OnInteract += delegate () { return Press(7); };
-        buttons[8].OnInteract += delegate () { return Press(8); };
-        buttons[9].OnInteract += delegate () { return Press(9); };
-        buttons[10].OnInteract += delegate () { return Press(10); };
-        buttons[11].OnInteract += delegate () { return Press(11); };
-        buttons[12].OnInteract += delegate () { return Press(12); };
-        buttons[13].OnInteract += delegate () { return Press(13); };
-        buttons[14].OnInteract += delegate () { return Press(14); };
-        buttons[15].OnInteract += delegate () { return Press(15); };
-
-        buttons[ 0].OnHighlight+=delegate(){color.text = (secondStage && points[ 0] != 999) ? colors[ 0] : ""; };
-        buttons[ 1].OnHighlight+=delegate(){color.text = (secondStage && points[ 1] != 999) ? colors[ 1] : ""; };
-        buttons[ 2].OnHighlight+=delegate(){color.text = (secondStage && points[ 2] != 999) ? colors[ 2] : ""; };
-        buttons[ 3].OnHighlight+=delegate(){color.text = (secondStage && points[ 3] != 999) ? colors[ 3] : ""; };
-        buttons[ 4].OnHighlight+=delegate(){color.text = (secondStage && points[ 4] != 999) ? colors[ 4] : ""; };
-        buttons[ 5].OnHighlight+=delegate(){color.text = (secondStage && points[ 5] != 999) ? colors[ 5] : ""; };
-        buttons[ 6].OnHighlight+=delegate(){color.text = (secondStage && points[ 6] != 999) ? colors[ 6] : ""; };
-        buttons[ 7].OnHighlight+=delegate(){color.text = (secondStage && points[ 7] != 999) ? colors[ 7] : ""; };
-        buttons[ 8].OnHighlight+=delegate(){color.text = (secondStage && points[ 8] != 999) ? colors[ 8] : ""; };
-        buttons[ 9].OnHighlight+=delegate(){color.text = (secondStage && points[ 9] != 999) ? colors[ 9] : ""; };
-        buttons[10].OnHighlight+=delegate(){color.text = (secondStage && points[10] != 999) ? colors[10] : ""; };
-        buttons[11].OnHighlight+=delegate(){color.text = (secondStage && points[11] != 999) ? colors[11] : ""; };
-        buttons[12].OnHighlight+=delegate(){color.text = (secondStage && points[12] != 999) ? colors[12] : ""; };
-        buttons[13].OnHighlight+=delegate(){color.text = (secondStage && points[13] != 999) ? colors[13] : ""; };
-        buttons[14].OnHighlight+=delegate(){color.text = (secondStage && points[14] != 999) ? colors[14] : ""; };
-        buttons[15].OnHighlight+=delegate(){color.text = (secondStage && points[15] != 999) ? colors[15] : ""; };
-
-        buttons[ 0].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 1].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 2].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 3].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 4].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 5].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 6].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 7].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 8].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[ 9].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[10].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[11].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[12].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[13].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[14].OnHighlightEnded += delegate () { color.text = "";};
-        buttons[15].OnHighlightEnded += delegate () { color.text = "";};
-        
+        for (int i1 = 0; i1 < 9; i1++)
+        {
+            int i = i1;
+            buttons[i].OnInteract += delegate{Press(i); return false;};
+            buttons[i].OnHighlight+=delegate{color.text = secondStage && points[i] != 999 ? colors[i] : ""; };
+            buttons[i].OnHighlightEnded += delegate{ color.text = "";};
+        }
     }
 #pragma warning disable 414
     private readonly string TwitchHelpMessage =
-        @"Use !{0} press # to press button on this position. Buttons are numbered 0-15 in reading order.";
+        "Use !{0} press # to press button on this position. Buttons are numbered 0-8 in reading order. Use !{0} hover # to hover the button.";
     private bool TwitchPlaysActive = false;
 #pragma warning restore 414
 
     IEnumerator ProcessTwitchCommand(string Command)
     {
+        yield return null;
         Command = Command.ToLower();
-        if (!Command.RegexMatch("press ([0-9])+")) yield return "sendtochaterror Invalid command!";
-        else
+        if (Command.RegexMatch("press ([0-9])+"))
         {
+            color.text = "";
             int? num = Command.Substring(6).TryParseInt();
-            if (num == null || num < 0 || num > 15) yield return "sendtochaterror Invalid command!";
+            if (num == null || num < 0 || num > 8) yield return "sendtochaterror Invalid command!";
             else Press((int)num);
         }
-        yield return null;
+        else if (Command.RegexMatch("hover ([0-9])+"))
+        {
+            int? num = Command.Substring(6).TryParseInt();
+            if (num == null || num < 0 || num > 8) yield return "sendtochaterror Invalid command!";
+            else buttons[(int)num].OnHighlight();
+        }
+        else yield return "sendtochaterror Invalid command!";
+        
     }
 
     IEnumerator TwitchHandleForcedSolve()
     {
         yield return null;
         Module.HandlePass();
-        string final = "YOUDIDITGOODJOB!";
-        for (int i = 0; i < 16; i++)
+        string final = "VAPSOLVED";
+        for (int i = 0; i < 9; i++)
         {
             texts[i].text = final[i].ToString();
             buttonMesh[i].material.color = new Color(0, 178 / 255f, 0);
